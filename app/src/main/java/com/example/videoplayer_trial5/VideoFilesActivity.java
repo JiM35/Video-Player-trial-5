3a455137a30b9f8403004c2ab31bfb72c93bd547
jamesgitonga <gitongajames35@gmail.com>
2023-12-15 19:04:45 +0300

    The thumbnails are not showing but video files, duration and video name are showing. Folder name is showing on the title. f you are checking the app on android 9 or below devices then your thumbnail will show. But if you are checking the app on android 10, then you have to change the target api for getting the thumbnail and also we have to change the color of the menu more.

--- app/src/main/java/com/example/videoplayer_trial5/VideoFilesActivity.java
+++ app/src/main/java/com/example/videoplayer_trial5/VideoFilesActivity.java
@@ -1,14 +1,63 @@
 package com.example.videoplayer_trial5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+
 
 public class VideoFilesActivity extends AppCompatActivity {
 
+    //    In VideoFilesAdapter we get the thumbnail and video duration now we will initialize the adapter in VideoFilesActivity.
+    RecyclerView recyclerView;
+    private ArrayList<MediaFiles> videoFilesArrayList = new ArrayList<>();
+    VideoFilesAdapter videoFilesAdapter;
+    String folder_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_files);
+        folder_name = getIntent().getStringExtra("folderName");  // The folder_name has to be above the getSupportActionBar(), if you are not doing this, you will get an error
+        getSupportActionBar().setTitle(folder_name);  // We have to set this folder_name on toolbar title
+        recyclerView = findViewById(R.id.videos_rv);
+        showVideoFiles();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void showVideoFiles() {
+        videoFilesArrayList = fetchMedia(folder_name);  // Pass the folder name in fetchMedia method
+        videoFilesAdapter = new VideoFilesAdapter(videoFilesArrayList, this);
+        recyclerView.setAdapter(videoFilesAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        videoFilesAdapter.notifyDataSetChanged();
+    }
+
+    private ArrayList<MediaFiles> fetchMedia(String folderName) {
+        ArrayList<MediaFiles> videoFiles = new ArrayList<>(); // This method wil return the media files
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Video.Media.DATA + " like?";  // DATA for path
+        String[] selectionArg = new String[]{"%" + folderName + "%"};  // Take String of Array type
+        Cursor cursor = getContentResolver().query(uri, null, selection, selectionArg, null);  // selection and selection argument will return the video files in a specific folder
+        if (cursor != null && cursor.moveToNext()) {
+            do {
+                @SuppressLint("Range") String id = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media._ID));
+                @SuppressLint("Range") String title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
+                @SuppressLint("Range") String displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                @SuppressLint("Range") String size = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                @SuppressLint("Range") String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                @SuppressLint("Range") String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                @SuppressLint("Range") String dateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_ADDED));
+                MediaFiles mediaFiles = new MediaFiles(id, title, displayName, size, duration, path, dateAdded);
+                videoFiles.add(mediaFiles);  // We have to add media files to video files. Add variable of modal class - mediaFiles.
+            } while (cursor.moveToNext());
+        }
+        return videoFiles;
     }
 }
---
+++ app/src/main/java/com/example/videoplayer_trial5/VideoFilesAdapter.java
@@ -0,0 +1,100 @@
+package com.example.videoplayer_trial5;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+// Adapter for showing video files - getting video files in a folder
+public class VideoFilesAdapter extends RecyclerView.Adapter<VideoFilesAdapter.ViewHolder> {
+    private final ArrayList<MediaFiles> videoList;
+    private final Context context;
+
+    public VideoFilesAdapter(ArrayList<MediaFiles> videoList, Context context) {
+        this.videoList = videoList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.video_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoFilesAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.videoName.setText(videoList.get(position).getDisplayName());  // Here we get the video name
+        String size = videoList.get(position).getSize();  // The size variable will return the size of video in bytes.
+        holder.videoSize.setText(android.text.format.Formatter.formatFileSize(context, Long.parseLong(size)));  // Now we have to change the byte size to MB or KB using formatter.
+        double milliSeconds = Double.parseDouble(videoList.get(position).getDuration());  // This variable contains the duration of video in milliseconds. We have to change the milliseconds into hours, minutes and seconds according to the duration of video. For this we have to create a method - timeConversion
+        holder.videoDuration.setText(timeConversion((long) milliSeconds));  // We will get the duration in milliseconds. Long value we will pass milliseconds
+        Glide.with(context).load(new File(videoList.get(position).getPath())).into(holder.thumbnail);  // For getting the thumbnail, we will use glide - that is image loading format
+        holder.menu_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "menu more", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Part 6 - 18:09, Part 7
+                Intent intent = new Intent(context, VideoPlayerActivity.class);  // When user clicks on video item, it will navigate to next activity using Intent. The second activity will be video player activity - we will have to create it.
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoList.size();
+    }  // Name of adapter is VideoFilesAdapter
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+//        Assign IDs to TextViews and ImageViews
+        ImageView thumbnail, menu_more;
+        TextView videoName, videoSize, videoDuration;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumbnail = itemView.findViewById(R.id.thumbnail);
+            menu_more = itemView.findViewById(R.id.video_menu_more);
+            videoName = itemView.findViewById(R.id.video_name);
+            videoSize = itemView.findViewById(R.id.video_size);
+            videoDuration = itemView.findViewById(R.id.video_duration);
+        }
+    }
+
+    //    We have to change the milliseconds into hours, minutes and seconds according to the duration of video. For this we have to create a method - timeConversion
+    @SuppressLint("DefaultLocale")
+    public String timeConversion(long value){
+        String videoTime;
+        int duration = (int) value;
+        int hrs = (duration / 3600000);  // 3600000 - milliseconds in one hour
+        int mns = (duration / 60000) % 60000; // 60000 - milliseconds in one minute
+        int scs = (duration % 60000 / 1000); // Divide by 1000 to change to seconds
+        if (hrs > 0) {
+            videoTime = String.format("%02d:%02d:%02d", hrs, mns, scs);  // If we do duration more than one hour, then we will use format - hour, minutes and seconds
+        } else {
+            videoTime = String.format("%02d:%02d", mns, scs);  // If video had duration less than 1 hour then we will use this format - minutes, seconds
+        }
+        return videoTime;
+    }
+}
+
+
+// For sending Arraylist through Intent, we will use Bundle. Before using bundle we have to implement media files - our model file. We have to implement it to pass label.
